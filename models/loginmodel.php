e1644bd4329fdc8b633848ad76d13bb502dad972
Marcos Rivas R <marcosrivasr@gmail.com>
2020-03-20 23:41:37 -0600

    ADD: encription validation for password

--- models/loginmodel.php
+++ models/loginmodel.php
@@ -9,11 +9,20 @@ class LoginModel extends Model{
     public function login($username, $password){
         // insertar datos en la BD
         try{
-            $query = $this->db->connect()->prepare('SELECT id, username, password, role FROM USERS WHERE USERNAME = :username AND PASSWORD = :password');
-            $query->execute(['username' => $username, 'password' => $password]);
+            //$query = $this->db->connect()->prepare('SELECT id, username, password, role FROM USERS WHERE USERNAME = :username AND PASSWORD = :password');
+            $query = $this->db->connect()->prepare('SELECT id, username, password, role FROM USERS WHERE USERNAME = :username');
+            //$query->execute(['username' => $username, 'password' => $password]);
+            $query->execute(['username' => $username]);
             
             if($row = $query->fetch(PDO::FETCH_ASSOC)){
-                return ['id' => $row['id'], 'username' => $row['username'], 'role' => $row['role']];
+                echo $password;
+                var_dump(password_verify($password, $row['password']));
+                if(password_verify($password, $row['password'])){
+                    return ['id' => $row['id'], 'username' => $row['username'], 'role' => $row['role']];
+                }else{
+                    return NULL;
+                }
+                
             }else{
                 return NULL;
             }
