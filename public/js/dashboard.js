937088f40b05022ae75bb76e93bb0be350144fff
Marcos Rivas R <marcosrivasr@gmail.com>
2020-04-04 00:21:36 -0600

    gráfica de barras

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,3 +3,24 @@ const newExpenseButton = document.querySelector('#new-expense-container');
 newExpenseButton.addEventListener('click', () =>{
     location.href = 'expenses/create';
 });
+
+google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Year', 'Sales', 'Expenses', 'Profit'],
+            ['2014', 1000, 400, 200],
+            ['2015', 1170, 460, 250],
+            ['2016', 660, 1120, 300],
+            ['2017', 1030, 540, 350]
+          ]);
+
+        var options = {
+              legend: { position: "none" }
+          };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
