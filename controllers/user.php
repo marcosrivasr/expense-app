2be383fa2f35805128f9b4cfba854652f5fd92c4
Marcos Rivas R <marcosrivasr@gmail.com>
2020-04-04 12:26:29 -0600

    metdos para datos de usuarios

--- controllers/user.php
+++ controllers/user.php
@@ -8,14 +8,132 @@ class User extends ControllerSession{
     }
 
     function render(){
-        $actual_link = trim("$_SERVER[REQUEST_URI]");
-        $url = explode('/', $actual_link);
-        $this->view->errorMessage = '';
-        $this->view->render('login/index');
+        $this->view->budget = $this->getBudget();
+        $this->view->name = $this->getName();
+        $this->view->photo = $this->getPhoto();
+        $this->view->render('dashboard/user');
     }
 
-    function obtenerBudget(){
-        return $this->model->get();
+    function getBudget(){
+        $id_user = $this->getUserSession()->getUserSessionData()['id'];
+        return $this->model->getBudget($id_user);
+    }
+    function getName(){
+        $id_user = $this->getUserSession()->getUserSessionData()['id'];
+        return $this->model->getName($id_user);
+    }
+
+    function getPhoto(){
+        $id_user = $this->getUserSession()->getUserSessionData()['id'];
+        return $this->model->getPhoto($id_user);
+    }
+
+    function updateBudget(){
+        if(!isset($_POST['budget'])){
+            header('location: ../');
+            return;
+        }
+
+        $budget = $_POST['budget'];
+
+        if(empty($budget) || $budget === 0 || $budget < 0){
+            header('location: '. constant('URL') . 'user');
+            return;
+        }
+        $id_user = $this->getUserSession()->getUserSessionData()['id'];
+        $this->model->updateBudget($budget, $id_user);
+        header('location: '. constant('URL') . 'user');
+    }
+
+    function updateName(){
+        if(!isset($_POST['name'])){
+            header('location: ../');
+            return;
+        }
+
+        $name = $_POST['name'];
+
+        if(empty($name)){
+            header('location: '. constant('URL') . 'user');
+            return;
+        }
+        $id_user = $this->getUserSession()->getUserSessionData()['id'];
+        $this->model->updateName($name, $id_user);
+        header('location: '. constant('URL') . 'user');
+    }
+
+    function updatePassword(){
+        if(!isset($_POST['current_password']) || !isset($_POST['new_password']) ){
+            header('location: ../');
+            return;
+        }
+
+        $current = $_POST['current_password'];
+        $new     = $_POST['new_password'];
+
+        if(empty($current) || empty($new)){
+            header('location: '. constant('URL') . 'user');
+            return;
+        }
+
+        if($current === $new){
+            header('location: '. constant('URL') . 'user');
+            return;
+        }
+
+        $id_user = $this->getUserSession()->getUserSessionData()['id'];
+
+        //validar que el current es el mismo que el guardado
+        if($this->model->comparePasswords($current, $id_user)){
+            //si lo es actualizar con el nuevo
+            $this->model->updatePassword($new, $id_user);
+            header('location: '. constant('URL') . 'user');
+        }else{
+            header('location: '. constant('URL') . 'user');
+            return;
+        }
+    }
+
+    function updatePhoto(){
+        if(!isset($_FILES['photo'])){
+            header('location: ../');
+            return;
+        }
+
+        $photo = $_FILES['photo'];
+
+        $target_dir = "public/img/photos/";
+        $extarr = explode('.',$photo["name"]);
+        $filename = $extarr[sizeof($extarr)-2];
+        $ext = $extarr[sizeof($extarr)-1];
+        $hash = md5(Date('Ymdgi') . $filename) . '.' . $ext;
+        $target_file = $target_dir . $hash;
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        
+        $check = getimagesize($photo["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($photo["tmp_name"], $target_file)) {
+                echo "The file ". basename( $photo["name"]). " has been uploaded.";
+                $id_user = $this->getUserSession()->getUserSessionData()['id'];
+                $this->model->updatePhoto($hash, $id_user);
+                header('location: '. constant('URL') . 'user');
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+        
     }
 }
 
--- models/usermodel.php
+++ models/usermodel.php
@@ -22,7 +22,116 @@ class UserModel extends Model{
         }catch(PDOException $e){
             return NULL;
         }
+    }
 
+    function getName($id){
+        try{
+            $query = $this->db->connect()->prepare('SELECT name FROM users WHERE id = :id');
+            $query->execute(['id' => $id]);
+
+            if($query->rowCount() > 0){
+                return $query->fetch(PDO::FETCH_ASSOC)['name'];
+            }else{
+                return NULL;
+            }
+
+        }catch(PDOException $e){
+            return NULL;
+        }
+    }
+
+    function getPhoto($id){
+        try{
+            $query = $this->db->connect()->prepare('SELECT photo FROM users WHERE id = :id');
+            $query->execute(['id' => $id]);
+
+            if($query->rowCount() > 0){
+                return $query->fetch(PDO::FETCH_ASSOC)['photo'];
+            }else{
+                return NULL;
+            }
+
+        }catch(PDOException $e){
+            return NULL;
+        }
+    }
+
+    function updateBudget($budget, $iduser){
+        try{
+            $query = $this->db->connect()->prepare('UPDATE users SET budget = :val WHERE id = :id');
+            $query->execute(['val' => $budget, 'id' => $iduser]);
+
+            if($query->rowCount() > 0){
+                return true;
+            }else{
+                return false;
+            }
+        
+        }catch(PDOException $e){
+            return NULL;
+        }
+    }
+
+    function updateName($name, $iduser){
+        try{
+            $query = $this->db->connect()->prepare('UPDATE users SET name = :val WHERE id = :id');
+            $query->execute(['val' => $name, 'id' => $iduser]);
+
+            if($query->rowCount() > 0){
+                return true;
+            }else{
+                return false;
+            }
+        
+        }catch(PDOException $e){
+            return NULL;
+        }
+    }
+
+    function updatePhoto($name, $iduser){
+        try{
+            $query = $this->db->connect()->prepare('UPDATE users SET photo = :val WHERE id = :id');
+            $query->execute(['val' => $name, 'id' => $iduser]);
+
+            if($query->rowCount() > 0){
+                return true;
+            }else{
+                return false;
+            }
+        
+        }catch(PDOException $e){
+            return NULL;
+        }
+    }
+
+    function updatePassword($new, $iduser){
+        try{
+            $hash = password_hash($new, PASSWORD_DEFAULT, ['cost' => 10]);
+            $query = $this->db->connect()->prepare('UPDATE users SET password = :val WHERE id = :id');
+            $query->execute(['val' => $hash, 'id' => $iduser]);
+
+            if($query->rowCount() > 0){
+                return true;
+            }else{
+                return false;
+            }
+        
+        }catch(PDOException $e){
+            return NULL;
+        }
+    }
+
+    function comparePasswords($current, $userid){
+        try{
+            $query = $this->db->connect()->prepare('SELECT id, password FROM USERS WHERE id = :id');
+            $query->execute(['id' => $userid]);
+            
+            if($row = $query->fetch(PDO::FETCH_ASSOC)) return password_verify($current, $row['password']);
+
+            return false;
+        }catch(PDOException $e){
+            return false;
+        }
     }
 }
 
