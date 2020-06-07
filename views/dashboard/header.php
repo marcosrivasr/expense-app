d6a0c44bd07dcb211148fd50465b5b5fb37b7cc9
Marcos Rivas R <marcosrivasr@gmail.com>
2020-06-06 20:34:58 -0500

    profile layout added

--- views/dashboard/header.php
+++ views/dashboard/header.php
@@ -1,21 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-    <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/default.css">
-    <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/dashboard.css">
-</head>
-<body>
-    
-    <div id="header">
-        <ul>
-            <li><a href="<?php echo constant('URL'); ?>expenses">Home</a></li>
-            <li><a href="<?php echo constant('URL').'expenses/'; ?>history">History</a></li>
-            <li><a href="<?php echo constant('URL'); ?>logout">Logout</a></li>
-        </ul>
+<link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/default.css">
+<link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/dashboard.css">
+
+
+<div id="header">
+    <ul>
+        <li><a href="<?php echo constant('URL'); ?>expenses">Home</a></li>
+        <li><a href="<?php echo constant('URL').'expenses/'; ?>history">History</a></li>
+        <li><a href="<?php echo constant('URL'); ?>logout">Logout</a></li>
+    </ul>
+
+    <div id="profile-container">
+        <a href="<?php echo constant('URL');?>user">
+            <div class="name"><?php echo $this->user['name']; ?></div>
+            <div class="photo">
+                <?php if($this->user['photo'] == ''){?>
+                        <i class="material-icons">account_circle</i>
+                <?php }else{ ?>
+                        <img src="<?php echo constant('URL'); ?>public/img/photos/<?php echo $this->user['photo'] ?>" width="32" />
+                <?php } ?>
+            </div>
+        </a>
+        <div id="submenu">
+            <ul>
+                <li><a href="<?php echo constant('URL'); ?>user">Ver perfil</a></li>
+                <li class='divisor'></li>
+                <li><a href="<?php echo constant('URL'); ?>logout">Cerrar sesión</a></li>
+            </ul>
+        </div>
     </div>
-</body>
-</html>
+</div>
