3080826586455184165428a533d333ddee223de3
Marcos Rivas R <marcosrivasr@gmail.com>
2020-07-12 14:04:52 -0500

    archivo para centralizar mensajes

---
+++ libs/messages.php
@@ -0,0 +1,12 @@
+<?php
+    function showError($message){
+        echo "<span class='error'>$message</span>";
+    }
+    function showInfo($message){
+        echo "<span class='info'>$message</span>";
+    }
+
+    function showSuccess($message){
+        echo "<span class='success'>$message</span>";
+    }
+?>
