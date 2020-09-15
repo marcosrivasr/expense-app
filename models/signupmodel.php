65c2f108e6219128731ea7f2efd709e865197c2a
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-15 17:02:42 -0500

    code removed

--- models/signupmodel.php
+++ models/signupmodel.php
@@ -5,19 +5,6 @@ class SignupModel extends Model{
     public function __construct(){
         parent::__construct();
     }
-
-    public function insert($username, $password){
-        // insertar datos en la BD
-        try{
-            $query = $this->db->connect()->prepare('INSERT INTO users (username, password, role, budget, photo, name) VALUES(:username, :password, "user", 0.0, "","" )');
-            $query->execute(['username' => $username, 'password' => $password]);
-            return true;
-        }catch(PDOException $e){
-            echo $e;
-            return false;
-        }
-        
-    }
 }
 
 ?>
