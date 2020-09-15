2c6a82e7383fc56feda26ea65df02ff3aafaeff8
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-15 17:21:10 -0500

    model functions added

--- libs/model.php
+++ libs/model.php
@@ -7,6 +7,14 @@ class Model{
     function __construct(){
         $this->db = new Database();
     }
+
+    function query($query){
+        return $this->db->connect()->query($query);
+    }
+
+    function prepare($query){
+        return $this->db->connect()->prepare($query);
+    }
 }
 
 ?>
