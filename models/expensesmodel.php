3207b63a3da3c604db7073b32be4779c0b8a8821
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-15 19:02:29 -0500

    new methods added

--- models/expensesmodel.php
+++ models/expensesmodel.php
@@ -1,10 +1,128 @@
 <?php
 
-class ExpensesModel extends Model{
+class ExpensesModel extends Model implements IModel{
+
+    private $id;
+    private $title;
+    private $amount;
+    private $categoryid;
+    private $date;
+    private $userid;
+
+    public function setId($id){ $this->id = $id; }
+    public function setTitle($title){ $this->title = $title; }
+    public function setAmount($amount){ $this->amount = $amount; }
+    public function setCategoryId($categoryid){ $this->categoryid = $categoryid; }
+    public function setDate($date){ $this->date = $date; }
+    public function setUserId($userid){ $this->userid = $userid; }
+
+    public function getId(){ return $this->id;}
+    public function getTitle(){ return $this->title; }
+    public function getAmount(){ return $this->amount; }
+    public function getCategoryId(){ return $this->categoryid; }
+    public function getDate(){ return $this->date; }
+    public function getUserId(){ return $this->userid; }
+
 
     public function __construct(){
         parent::__construct();
     }
+
+    public function save(){
+        try{
+            $query = $this->prepare('INSERT INTO expenses (title, amount, category_id, date, id_user) VALUES(:title, :amount, :category, :d, :user)');
+            $query->execute([
+                'title' => $this->title, 
+                'amount' => $this->amount, 
+                'category' => $this->categoryid, 
+                'user' => $this->userid, 
+                'd' => $this->date
+            ]);
+            if($query->rowCount()) return true;
+
+            return false;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+    public function getAll(){
+        $items = [];
+
+        try{
+            $query = $this->query('SELECT * FROM expenses');
+
+            while($p = $query->fetch(PDO::FETCH_ASSOC)){
+                /* $item = new ExpensesModel();
+                $item->setId($p['id']);
+                $item->setTitle($p['title']);
+                $item->setAmount($p['amount']);
+                $item->setCategoryId($p['category_id']);
+                $item->setDate($p['date']);
+                $item->setUserId($p['id_user']); */
+                $item = new ExpensesModel();
+                $item->from($p); 
+                
+                array_push($items, $item);
+            }
+
+            return $items;
+
+        }catch(PDOException $e){
+            echo $e;
+        }
+    }
+    public function get($id){
+        try{
+            $query = $this->prepare('SELECT * FROM expenses WHERE id = :id');
+            $query->execute([ 'id' => $id]);
+            $user = $query->fetch(PDO::FETCH_ASSOC);
+
+            $this->from($user);
+
+            return $this;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+    public function delete($id){
+        try{
+            $query = $this->db->connect()->prepare('DELETE FROM users WHERE id = :id');
+            $query->execute([ 'id' => $id]);
+            return true;
+        }catch(PDOException $e){
+            echo $e;
+            return false;
+        }
+    }
+
+    public function update(){
+        try{
+            $query = $this->db->connect()->prepare('UPDATE expenses SET title = :title, amount = :amount, category_id = :category, date = :d, id_user = :user WHERE id = :id');
+            $query->execute([
+                'title' => $this->title, 
+                'amount' => $this->amount, 
+                'category' => $this->categoryid, 
+                'user' => $this->userid, 
+                'd' => $this->date
+            ]);
+            return true;
+        }catch(PDOException $e){
+            echo $e;
+            return false;
+        }
+    }
+
+    public function from($array){
+        $this->id = $array['id'];
+        $this->title = $array['title'];
+        $this->amount = $array['amount'];
+        $this->categoryid = $array['category_id'];
+        $this->date = $array['date'];
+        $this->userid = $array['id_user'];
+    }
+
+
+
 
     function insert($title, $amount, $category, $date, $id_user){
         try{
