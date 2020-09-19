062875a167c6c74e18d188c591bf976d1b408dff
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-18 23:50:08 -0500

    FIXED: require to require_once

--- libs/controller.php
+++ libs/controller.php
@@ -10,7 +10,7 @@ class Controller{
         $url = 'models/'.$model.'model.php';
 
         if(file_exists($url)){
-            require $url;
+            require_once $url;
 
             $modelName = $model.'Model';
             $this->model = new $modelName();
