27f2f8149a577097764342d5d4ff5c320b0faeff
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-18 23:44:09 -0500

    classes added

--- index.php
+++ index.php
@@ -19,13 +19,18 @@ require_once 'libs/view.php';
 require_once 'libs/model.php';
 require_once 'libs/app.php';
 
+
 require_once 'classes/session.php';
 require_once 'classes/sessionController.php';
+require_once 'classes/errors.php';
+require_once 'classes/success.php';
+
 
 require_once 'config/config.php';
 require_once 'classes/AuthSites.php';
 
-include_once "models/user.php";
+include_once 'models/usermodel.php';
+include_once 'models/expensesmodel.php';
 include_once "models/categoriesmodel.php";
 include_once "models/joinexpensescategoriesmodel.php";
 
