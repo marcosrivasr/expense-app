39936d88dffcb5e6e7837683c35c413c4c677469
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-18 23:50:42 -0500

    ADDED: messages handler in View

--- libs/view.php
+++ libs/view.php
@@ -7,8 +7,68 @@ class View{
 
     function render($nombre, $data = []){
         $this->d = $data;
-        error_log("View::render(): data: " . count($this->d) . ' items');
+        
+        $this->handleMessages();
+        
+
         require 'views/' . $nombre . '.php';
+    }
+    
+    private function handleMessages(){
+        if(isset($_GET['success']) && isset($_GET['error'])){
+            // no se muestra nada porque no puede haber un error y success al mismo tiempo
+        }else if(isset($_GET['success'])){
+            
+            $this->handleSuccess();
+        }else if(isset($_GET['error'])){
+            $this->handleError();
+        }
+    }
+
+    private function handleError(){
+        if(isset($_GET['error'])){
+            $hash = $_GET['error'];
+            $errors = new Errors();
+
+            if($errors->existsKey($hash)){
+                error_log('View::handleError() existsKey =>' . $errors->existsKey($hash));
+                $this->d['error'] = $errors->get($hash);
+            }else{
+                $this->d['error'] = NULL;
+            }
+        }
+    }
+
+
+    private function handleSuccess(){
+        if(isset($_GET['success'])){
+            $hash = $_GET['success'];
+            $success = new Success();
+
+            if($success->existsKey($hash)){
+                error_log('View::handleError() existsKey =>' . $success->existsKey($hash));
+                $this->d['success'] = $success->get($hash);
+            }else{
+                $this->d['success'] = NULL;
+            }
+        }
+    }
+
+    public function showMessages(){
+        $this->showError();
+        $this->showSuccess();
+    }
+
+    public function showError(){
+        if(array_key_exists('error', $this->d)){
+            echo '<div class="error">'.$this->d['error'].'</div>';
+        }
+    }
+
+    public function showSuccess(){
+        if(array_key_exists('success', $this->d)){
+            echo '<div class="success">'.$this->d['success'].'</div>';
+        }
     }
 }
 
