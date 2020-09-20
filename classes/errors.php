b991266bfd373a8c3dac5cfebc420f1c887a8ecc
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-20 12:12:16 -0500

    ADDED: signup errors and success messages

--- classes/errors.php
+++ classes/errors.php
@@ -23,6 +23,9 @@ class Errors{
     const ERROR_LOGIN_AUTHENTICATE               = "11c37cfab311fbe28652f4947a9523c4";
     const ERROR_LOGIN_AUTHENTICATE_EMPTY         = "2194ac064912be67fc164539dc435a42";
     const ERROR_LOGIN_AUTHENTICATE_DATA          = "bcbe63ed8464684af6945ad8a89f76f8";
+    const ERROR_SIGNUP_NEWUSER                   = "1fdce6bbf47d6b26a9cd809ea1910222";
+    const ERROR_SIGNUP_NEWUSER_EMPTY             = "a5bcd7089d83f45e17e989fbc86003ed";
+    const ERROR_SIGNUP_NEWUSER_EXISTS            = "a74accfd26e06d012266810952678cf3";
 
 
     private $errorsList = [];
@@ -46,6 +49,9 @@ class Errors{
             Errors::ERROR_LOGIN_AUTHENTICATE        => 'Hubo un problema al autenticarse',
             Errors::ERROR_LOGIN_AUTHENTICATE_EMPTY  => 'Los parámetros para autenticar no pueden estar vacíos',
             Errors::ERROR_LOGIN_AUTHENTICATE_DATA   => 'Nombre de usuario y/o password incorrectos',
+            Errors::ERROR_SIGNUP_NEWUSER            => 'Hubo un error al intentar registrarte. Intenta de nuevo',
+            Errors::ERROR_SIGNUP_NEWUSER_EMPTY      => 'Los campos no pueden estar vacíos',
+            Errors::ERROR_SIGNUP_NEWUSER_EXISTS     => 'El nombre de usuario ya existe, selecciona otro',
         ];
     }
 
--- classes/success.php
+++ classes/success.php
@@ -13,6 +13,7 @@ class Success{
     const SUCCESS_USER_UPDATENAME       = "6fb34a5e4118fb823636ca24a1d21669";
     const SUCCESS_USER_UPDATEPASSWORD       = "6fb34a5e4118fb823636ca24a1d21669";
     const SUCCESS_USER_UPDATEPHOTO       = "edabc9e4581fee3f0056fff4685ee9a8";
+    const SUCCESS_SIGNUP_NEWUSER       = "8281e04ed52ccfc13820d0f6acb0985a";
     
     private $successList = [];
 
@@ -25,7 +26,8 @@ class Success{
             Success::SUCCESS_USER_UPDATEBUDGET => "Presupuesto actualizado correctamente",
             Success::SUCCESS_USER_UPDATENAME => "Nombre actualizado correctamente",
             Success::SUCCESS_USER_UPDATEPASSWORD => "Contraseña actualizado correctamente",
-            Success::SUCCESS_USER_UPDATEPHOTO => "Imagen de usuario actualizada correctamente"
+            Success::SUCCESS_USER_UPDATEPHOTO => "Imagen de usuario actualizada correctamente",
+            Success::SUCCESS_SIGNUP_NEWUSER => "Usuario registrado correctamente"
         ];
     }
 
