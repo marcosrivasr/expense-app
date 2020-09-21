0d3b6323360fda6f51e1c116ceb1236e3c50ce2a
Marcos Rivas <marcosrivasr@gmail.com>
2020-09-21 15:09:45 -0500

    ADDED: dashboard methods

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -3,19 +3,63 @@
 
 class Dashboard extends SessionController{
 
+    private $user;
+
     function __construct(){
         parent::__construct();
+
+        $this->user = $this->getUserSessionData();
+        error_log("Dashboard::constructor() ");
     }
 
-    function render(){
-        $this->view->render('dashboard/index');
+     function render(){
+        error_log("Dashboard::RENDER() ");
+        $expensesModel          = new ExpensesModel();
+        $expenses               = $this->getExpenses(5);
+        $totalThisMonth         = $expensesModel->getTotalAmountThisMonth($this->user->getId());
+        $maxExpensesThisMonth   = $expensesModel->getMaxExpensesThisMonth($this->user->getId());
+        $categories             = $this->getCategories();
+
+        $this->view->render('dashboard/index', [
+            'user'                 => $this->user,
+            'expenses'             => $expenses,
+            'totalAmountThisMonth' => $totalThisMonth,
+            'maxExpensesThisMonth' => $maxExpensesThisMonth,
+            'categories'           => $categories
+        ]);
+    }
+    
+    //obtiene la lista de expenses y $n tiene el número de expenses por transacción
+    private function getExpenses($n = 0){
+        if($n < 0) return NULL;
+        error_log("Dashboard::getExpenses() id = " . $this->user->getId());
+        $expenses = new ExpensesModel();
+        return $expenses->getByUserIdAndLimit($this->user->getId(), $n);   
     }
 
-    function saludo(){
-        $actual_link = trim("$_SERVER[REQUEST_URI]");
-        $url = explode('/', $actual_link);
-        echo $url[2];
+    function getCategories(){
+        $res = [];
+        $categoriesModel = new CategoriesModel();
+        $expensesModel = new ExpensesModel();
 
+        $categories = $categoriesModel->getAll();
+
+        foreach ($categories as $category) {
+            $categoryArray = [];
+            //obtenemos la suma de amount de expenses por categoria
+            $total = $expensesModel->getTotalByCategoryThisMonth($category->getId(), $this->user->getId());
+            // obtenemos el número de expenses por categoria por mes
+            $numberOfExpenses = $expensesModel->getNumberOfExpensesByCategoryThisMonth($category->getId(), $this->user->getId());
+            
+            if($numberOfExpenses > 0){
+                $categoryArray['total'] = $total;
+                $categoryArray['count'] = $numberOfExpenses;
+                $categoryArray['category'] = $category;
+                array_push($res, $categoryArray);
+            }
+            
+        }
+        return $res;
     }
 }
 
